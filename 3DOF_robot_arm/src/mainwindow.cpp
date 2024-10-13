b857290432bae0364f01fcefec3e45c83545ec21
kinesis19 <lordkinesis@naver.com>
2024-10-13 10:05:06 +0900

    :sparkles: Feat: Create btn_generate cliked method
    btn_generate 클릭시, spinbox에 있는 값들을 terminal에 띄우는 것까지 처리함.

--- 3DOF_robot_arm/src/mainwindow.cpp
+++ 3DOF_robot_arm/src/mainwindow.cpp
@@ -13,3 +13,19 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+// When Clicked the btn_generate
+void MainWindow::on_btn_generate_clicked()
+{
+
+    // Getting the value from link1 to link3
+    int link1_value = ui->spinBox_1->text().toInt();
+    int link2_value = ui->spinBox_2->text().toInt();
+    int link3_value = ui->spinBox_3->text().toInt();
+
+    // for Debugging
+    qDebug() << "Link1: " << link1_value;
+    qDebug() << "Link2: " << link2_value;
+    qDebug() << "Link3: " << link3_value;
+
+}
