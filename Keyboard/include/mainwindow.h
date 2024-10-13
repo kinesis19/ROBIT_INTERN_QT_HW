61846f626452efc45bccf7cf77a02c66f2af3968
kinesis19 <lordkinesis@naver.com>
2024-10-13 20:19:42 +0900

    :hammer: Add: Button Clicked() Method
    버튼 메소드 go to slot까지맘ㄴ 진행함. 잠을 못 자니까 너무 아프다

--- Keyboard/include/mainwindow.h
+++ Keyboard/include/mainwindow.h
@@ -17,6 +17,133 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btn_copy_clicked();
+
+    void on_btn_clear_clicked();
+
+    void on_btn_tilde_clicked();
+
+    void on_btn_exclamation_clicked();
+
+    void on_btn_at_clicked();
+
+    void on_btn_hash_clicked();
+
+    void on_btn_dollar_clicked();
+
+    void on_btn_percent_clicked();
+
+    void on_btn_caret_clicked();
+
+    void on_btn_ampersand_clicked();
+
+    void on_btn_asterisk_clicked();
+
+    void on_btn_leftParenthesis_clicked();
+
+    void on_btn_rightParenthesis_clicked();
+
+    void on_btn_minus_clicked();
+
+    void on_btn_plus_clicked();
+
+    void on_btn_backSpace_clicked();
+
+    void on_btn_tab_clicked();
+
+    void on_btn_q_clicked();
+
+    void on_btn_w_clicked();
+
+    void on_btn_e_clicked();
+
+    void on_btn_r_clicked();
+
+    void on_btn_t_clicked();
+
+    void on_btn_y_clicked();
+
+    void on_btn_u_clicked();
+
+    void on_btn_i_clicked();
+
+    void on_btn_o_clicked();
+
+    void on_btn_p_clicked();
+
+    void on_btn_leftCurlyBrace_clicked();
+
+    void on_btn_rightCurlyBrace_clicked();
+
+    void on_btn_backSlash_clicked();
+
+    void on_btn_caps_clicked();
+
+    void on_btn_a_clicked();
+
+    void on_btn_s_clicked();
+
+    void on_btn_d_clicked();
+
+    void on_btn_f_clicked();
+
+    void on_btn_g_clicked();
+
+    void on_btn_h_clicked();
+
+    void on_btn_j_clicked();
+
+    void on_btn_k_clicked();
+
+    void on_btn_l_clicked();
+
+    void on_btn_colon_clicked();
+
+    void on_btn_doubleQuote_clicked();
+
+    void on_btn_enter_clicked();
+
+    void on_btn_shiftLeft_clicked();
+
+    void on_btn_z_clicked();
+
+    void on_btn_x_clicked();
+
+    void on_btn_c_clicked();
+
+    void on_btn_v_clicked();
+
+    void on_btn_b_clicked();
+
+    void on_btn_n_clicked();
+
+    void on_btn_m_clicked();
+
+    void on_btn_lessThan_clicked();
+
+    void on_btn_greaterThan_clicked();
+
+    void on_btn_questionMark_clicked();
+
+    void on_btn_shiftRight_clicked();
+
+    void on_btn_ctrlLeft_clicked();
+
+    void on_btn_winLeft_clicked();
+
+    void on_btn_altLeft_clicked();
+
+    void on_btn_space_clicked();
+
+    void on_btn_lang_clicked();
+
+    void on_btn_winRight_clicked();
+
+    void on_btn_menu_clicked();
+
+    void on_btn_ctrlRight_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
--- Keyboard/src/mainwindow.cpp
+++ Keyboard/src/mainwindow.cpp
@@ -14,3 +14,382 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_btn_copy_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_clear_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_tilde_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_exclamation_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_at_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_hash_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_dollar_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_percent_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_caret_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_ampersand_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_asterisk_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_leftParenthesis_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_rightParenthesis_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_minus_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_plus_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_backSpace_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_tab_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_q_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_w_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_e_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_r_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_t_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_y_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_u_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_i_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_o_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_p_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_leftCurlyBrace_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_rightCurlyBrace_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_backSlash_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_caps_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_a_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_s_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_d_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_f_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_g_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_h_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_j_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_k_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_l_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_colon_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_doubleQuote_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_enter_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_shiftLeft_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_z_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_x_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_c_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_v_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_b_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_n_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_m_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_lessThan_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_greaterThan_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_questionMark_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_shiftRight_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_ctrlLeft_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_winLeft_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_altLeft_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_space_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_lang_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_winRight_clicked()
+{
+
+
+}
+
+
+void MainWindow::on_btn_menu_clicked()
+{
+
+}
+
+
+void MainWindow::on_btn_ctrlRight_clicked()
+{
+
+}
+
